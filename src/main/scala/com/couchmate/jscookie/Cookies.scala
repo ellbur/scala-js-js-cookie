c0d70d440222c11ba61db299f77d1f3a3d706a96
Matt <mjo2089@gmail.com>
2016-03-16 11:38:39 -0400

    added options to gets

--- src/main/scala/com/couchmate/jscookie/Cookies.scala
+++ src/main/scala/com/couchmate/jscookie/Cookies.scala
@@ -5,9 +5,9 @@ import scala.scalajs.js
 @js.native
 object Cookies extends js.Object {
   def get(): String = js.native
-  def get(name: String): String = js.native
+  def get(name: String): js.UndefOr[String] = js.native
   def getJSON(): js.Any = js.native
-  def getJSON(name: String): js.Any = js.native
+  def getJSON(name: String): js.UndefOr[js.Any] = js.native
 
   def set(name: String, value: String): Unit = js.native
   def set(name: String, value: String, opts: CookieOpts): Unit = js.native
